66a24139dc68abece706fe21cb9c9cc6ebd178af
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2024-01-25 16:49:52 +0330

    Feature: Add about-me

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,14 @@
+import Link from "next/link";
+
 import Links from "./links/Links";
 import styles from "./Navbar.module.css";
 
 function Navbar() {
   return (
     <nav className={styles.container}>
-      <div className={styles.logo}>Logo</div>
+      <Link href="/" className={styles.logo}>
+        Logo
+      </Link>
       <div>
         <Links />
       </div>
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,42 +1,64 @@
+"use client";
+
+import { useState } from "react";
+import { Menu } from "lucide-react";
+
 import styles from "./Links.module.css";
 import NavLink from "./navlink/NavLink";
 
-function Links() {
-  const links = [
-    {
-      title: "Home",
-      path: "/",
-    },
-    {
-      title: "About me",
-      path: "/about",
-    },
-    {
-      title: "Contact me",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
+//! temporary
+const session = true;
+const isAdmin = true;
 
-  //! temporary
-  const session = true;
-  const isAdmin = true;
+// ? NavLinks
+const links = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About me",
+    path: "/about",
+  },
+  {
+    title: "Contact me",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
+
+function Links() {
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}
-      {session ? (
-        <>
-          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
-          <button className={styles.logout}>Logout</button>
-        </>
-      ) : (
-        <NavLink item={{ title: "Login", path: "/login" }} />
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <Menu
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      />
+
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
       )}
     </div>
   );
